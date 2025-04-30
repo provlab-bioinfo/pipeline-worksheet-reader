d757571dfadcf66925049e85e92e040f2e29aea7
knacko <geekbike@hotmail.com>
2025-04-30 14:28:36 -0600

    feat: subsets worksheets to specific group

--- pipeline_worksheet_reader/functions.py
+++ pipeline_worksheet_reader/functions.py
@@ -1,5 +1,6 @@
 import os, re, io, pickle, glob, random, copy, shutil, logging, errno, hashlib, gzip
 import pandas as pd
+import openpyxl as xl
 from configparser import ConfigParser
 from pathlib import Path
 from contextlib import suppress
@@ -7,14 +8,13 @@ from alive_progress import alive_bar
 from itertools import chain
 from collections import defaultdict
 
-class pipelineWorksheet:
+class PipelineWorksheet:
 
     def __init__(self, path):
         self.path = path
 
     def getDataVar(self:object, section:str):
         """Generates a dictionary from the first two columns of a [HEADER] section
-        :param path: The path to the reference file
         :param section: The name of the section
         :return: A dictionary containing variable keys
         """    
@@ -27,7 +27,6 @@ class pipelineWorksheet:
 
     def getDataFrame(self:object, section:str):
         """Generates a DataFrame from a [HEADER] section
-        :param path: The path to the reference file
         :param section: The name of the section
         :return: A DataFrame representing the sections
         """ 
@@ -40,3 +39,21 @@ class pipelineWorksheet:
         df = pd.read_csv(buf)
         return (df)
     
+    # def convertToSampleSheet(self:object):
+
+    def subsetWorksheet(self:object, group:str, path:str):
+        """Generates a DataFrame from a [HEADER] section
+        :param group: The group to look for in the column 'Sample_Group'
+        :param path: The output path of the subsetted pipeline worksheet
+        :return: A DataFrame representing the sections
+        """ 
+        wb = xl.load_workbook(self.path)
+        ws = wb.active
+        rows = list(ws.iter_rows(min_row=1, max_row=ws.max_row))
+        for row in reversed(rows): 
+            cell = row[2] # col idx 3 is Sample_Group
+            if cell.value == "Sample_Group":
+                break
+            if cell.value != group:
+                ws.delete_rows(cell.row, 1)
+        wb.save(path)
